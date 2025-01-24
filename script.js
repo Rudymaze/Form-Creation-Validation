4522f3ff481d4c2ae8371c47a04055b1aac858d3
RUDYMAZE <njockrudiger4@gmail.com>
2025-01-25 00:33:00 +0200

    adjustedments

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let isValid = true;
     let message = [];
 
+    // USERNAME ELENMENT AND INPUT FIELD
     const usernameInput = document.getElementById("username");
     const username = usernameInput.value.trim();
     if (username.length < 3) {
@@ -15,6 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
       isValid = false;
     }
 
+    // EMAIL ELEMENT AND INPUT FIELD
     const emailInput = document.getElementById("email");
     const email = emailInput.value.trim();
     if (!email.incudes("@") || !email.includes(".")) {
@@ -22,11 +24,24 @@ document.addEventListener("DOMContentLoaded", function () {
       isValid = false;
     }
 
+    // PASSWORD ELEMENT AND INPUT FIELD
     const passwordInput = document.getElementById("password");
     const password = passwordInput.value.trim();
     if (password.length < 8) {
       message.push("Password must be at least 8 characters long.");
       isValid = false;
     }
+
+    feedbackDiv.style.display = "block";
+
+    if (isValid) {
+      // Success message
+      feedbackDiv.textContent = "Registration successful!";
+      feedbackDiv.style.color = "#28a745"; // Green color for success
+    } else {
+      feedbackDiv.innerHTML = messages.join("<br>");
+      feedbackDiv.style.color = "#dc3545"; // Red color for errors
+    }
   });
+  form.addEventListener("submit", form);
 });
